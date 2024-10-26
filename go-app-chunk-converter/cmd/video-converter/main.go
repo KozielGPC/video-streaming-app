65ddc2098e55bf4533048d9f3ec954fabdcc4880
Márcio Gabriel <marcio.campos@luizalabs.com>
2024-10-26 10:25:02 -0300

    feat: add go video chunk converter

---
+++ go-app-chunk-converter/cmd/video-converter/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	err := mergeChunks("mediatest/chunks", "merged.mp4")
+	fmt.Printf("Error: %s\n", err)
+}
+
+func extractNumber(fileName string) int {
+	re := regexp.MustCompile(`\d+`)
+	numStr := re.FindString(filepath.Base(fileName))
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return -1
+	}
+	return num
+}
+
+func mergeChunks(inputDir, outputFile string) error {
+	chunks, err := filepath.Glob(filepath.Join(inputDir, "*.chunk"))
+
+	fmt.Printf("chunks: %s\n", chunks)
+	if err != nil {
+		return fmt.Errorf("failed to find chunks: %v", err)
+	}
+
+	sort.Slice(chunks, func(i, j int) bool {
+		return extractNumber(chunks[i]) < extractNumber(chunks[j])
+	})
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+
+	defer output.Close()
+
+	for _, chunk := range chunks {
+		input, err := os.Open(chunk)
+		if err != nil {
+			return fmt.Errorf("failed to open chunk file: %v", err)
+		}
+
+		_, err = output.ReadFrom(input)
+		if err != nil {
+			return fmt.Errorf("failed to write chunk %s to merged file: %v", chunk, err)
+		}
+		input.Close()
+	}
+
+	return nil
+}
