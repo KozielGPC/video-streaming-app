907984362b6db9992fb288ca9b1bf971cfc93dd1
Marcio Gabriel <marcio.gabriel.contractor@acorns.com>
2025-06-20 11:05:44 -0300

    feat: display video in video page

--- microfrontends/next-app/src/pages/[slug]/getVideo.ts
+++ microfrontends/next-app/src/pages/[slug]/getVideo.ts
@@ -1,14 +1,17 @@
 import { VideoModel } from "@/pages/types/video";
-import { getMockedVideos } from "@/utils";
 
 export function getVideo(slug: string): VideoModel {
-  //   const response = await fetch(`${process.env.DJANGO_API_URL}/videos/${slug}`, {
-  //     //cache: "no-cache",
-  //     next: {
-  //     //     revalidate: 60 * 5
-  //         tags: [`video-${slug}`]
-  //     }
-  //   });
-  //   return response.json();
-  return getMockedVideos()[0];
+  return {
+    id: 1,
+    title: "Big Buck Bunny",
+    description:
+      "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.",
+    thumbnail: "https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217",
+    slug: "big-buck-bunny",
+    published_at: new Date().toISOString(),
+    likes: 100,
+    views: 1000,
+    tags: ["cartoon", "bunny"],
+    video_url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  };
 }
